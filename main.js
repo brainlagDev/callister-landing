652c050008621206f3e67b07702283687b6f31b9
Nikita Horshkov <jetnekit@gmail.com>
2025-08-07 19:55:57 +0300

    [Master] Added some basic animations

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function handleClick() {
-  alert("Thanks for clicking!");
-}
-
-function scrollToSection(id) {
+function scrollToSection(id) 
+{
   document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+  const topBar = document.querySelector('.top-bar');
+  const gamesSection = document.getElementById('games');
+
+  function checkHeader() 
+  {
+    const gamesTop = gamesSection.getBoundingClientRect().top + window.scrollY;
+    if (window.scrollY > gamesTop - 80) 
+      { 
+      topBar.classList.add('hide');
+    } else {
+      topBar.classList.remove('hide');
+    }
+  }
+
+  window.addEventListener('scroll', checkHeader);
+});
